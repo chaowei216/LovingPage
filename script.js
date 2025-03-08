56ec6427aae6d75bd5d676edfd003edddcb07ddb
chaoweiz <luutrieuvi2003@gmail.com>
2025-03-08 21:25:51 +0700

    update

--- script.js
+++ script.js
@@ -8,72 +8,72 @@ btnAction.addEventListener("click", handleClick);
 function handleClick() {
   if (btnClickCount == 0) {
     showText("Hôm nay là 8/3 á...");
-    showImg("imgs/img1.gif");
+    showImg("imgs/img1.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 1) {
     showText("Nên mình muốn nói rằng...");
-    showImg("imgs/begin.gif");
+    showImg("imgs/begin.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 2) {
     showText("Bạn là 1 người xinh đẹp và cuốn hút nhất mình từng gặp!!!");
-    showImg("imgs/img2.gif");
+    showImg("imgs/img2.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 3) {
     showText("Nên là mong bạn sẽ luôn vui vẻ và tươi cười thật nhiềuuuu");
-    showImg("imgs/img3.gif");
+    showImg("imgs/img3.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 4) {
     showText("À chúc vậy thui, giờ là trải lòng ha !!");
-    showImg("imgs/img4.gif");
+    showImg("imgs/img4.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 5) {
     showText("Mình là người không tự tin về bản thân rất nhiều");
-    showImg("imgs/img5.gif");
+    showImg("imgs/img5.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 6) {
     showText("Nhưng mà từ khi gặp bạn thì mọi thứ thay đổi rồi á");
-    showImg("imgs/img6.gif");
+    showImg("imgs/img6.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 7) {
     showText(
       "Mình tự tin hơn và làm những thứ mình tưởng rằng mình không làm nổi"
     );
-    showImg("imgs/img7.gif");
+    showImg("imgs/img7.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 8) {
     showText("Nên là với mình thì bạn là một người rất đặc biệt");
-    showImg("imgs/img8.gif");
+    showImg("imgs/img8.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 9) {
     showText(
       "Mặc dù mình năng động nhưng lại rất ghét phải nghe người khác nói quá nhiều"
     );
-    showImg("imgs/img9.gif");
+    showImg("imgs/img9.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 10) {
     showText("Nhưng không hiểu sao bạn lại là 1 ngoại lệ");
-    showImg("imgs/img10.gif");
+    showImg("imgs/img10.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 11) {
     showText("Mình còn nghĩ là 1 ngày đi 8 tiếng để nghe còn chưa đủ nữa");
-    showImg("imgs/img11.gif");
+    showImg("imgs/img11.webp");
     showBtn("Tiếp tục");
     btnClickCount++;
   } else if (btnClickCount == 12) {
     showText("Nên là ờm...........");
-    showImg("imgs/img12.gif");
-    btn.classList.add("hide");
+    showImg("imgs/img12.webp");
+    hideBtn();
     btnClickCount++;
   }
 }
@@ -84,7 +84,7 @@ function showText(text) {
     setTimeout(() => {
       title.innerHTML = text;
       title.style.opacity = "1";
-    }, 200);
+    }, 300);
   }, 300);
 }
 
@@ -94,7 +94,7 @@ function showImg(imgSrc) {
     setTimeout(() => {
       img.src = imgSrc;
       img.style.opacity = "1";
-    }, 200);
+    }, 300);
   }, 300);
 }
 
@@ -104,6 +104,12 @@ function showBtn(btnText) {
     setTimeout(() => {
       btn.classList.remove("hide");
       btn.innerHTML = btnText;
-    }, 200);
+    }, 300);
+  }, 300);
+}
+
+function hideBtn() {
+  setTimeout(() => {
+    btn.classList.add("hide");
   }, 300);
 }
